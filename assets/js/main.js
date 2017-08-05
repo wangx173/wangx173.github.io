780e580632fb8d31534ec0cba47920fec5660efc
wangx173 <rogerxiang.wang@gmail.com>
2017-08-05 02:26:11 -0400

    nav bar added

--- assets/js/main.js
+++ assets/js/main.js
@@ -141,7 +141,7 @@
 
 
 $(function() {
-	$('a[href*=#]').on('click', function(e) {
+	$('.nav-item').on('click', function(e) {
 		e.preventDefault();
 		$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
 	});
